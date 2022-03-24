4e28cfaf5f59eb9ca1ffa68031c49af49ca0d808
PedroMDAndre <pedro.mda@gmail.com>
2022-03-24 19:50:18 +0000

    Spring project structure

---
+++ backend/src/main/java/pt/calculator/tax/TaxApplication.java
@@ -0,0 +1,13 @@
+package pt.calculator.tax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pt/calculator/tax/TaxApplicationTests.java
@@ -0,0 +1,14 @@
+package pt.calculator.tax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// placeholder
+	}
+
+}
