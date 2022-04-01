2d59d446d6c85b55142647f031d27da6bde4c084
peterark <68342045+peterark@users.noreply.github.com>
2022-04-01 23:01:01 +0100

    radio buttons

--- frontend/purchase-data/src/app/calculator/calculator.component.ts
+++ frontend/purchase-data/src/app/calculator/calculator.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { CalculatorService } from '../calculator-service/calculator.service';
 import { PurchaseDataDto } from '../model/purchase-data-dto';
 
@@ -8,34 +8,52 @@ import { PurchaseDataDto } from '../model/purchase-data-dto';
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit {
 
   purchaseDataDto: PurchaseDataDto = {
     netValue: "100",
     grossValue: undefined,
     vatValue: undefined,
     vatRate: "0.2"
-  };
+  }; // start with no values
 
-  taxRateList: number[] = [0.1, 0.13, 0.2];
+  taxRateList: number[] = [0.1, 0.13, 0.2]; // to be retrieved from API
 
   errorMessage: string = '';
 
- calculateForm: FormGroup = this.formBuilder.group({
-    netValue: {value: undefined, disabled: true},
-    grossValue: {value: undefined, disabled: true},
-    vatValue: {value: undefined, disabled: true},
-    vatRate: {value: undefined}
+
+  private netValue: FormControl = new FormControl('');
+  private grossValue: FormControl = new FormControl('');
+  private vatValue: FormControl = new FormControl('');
+  private vatRate: FormControl = new FormControl('');
+  private radioInputData: FormControl = new FormControl('net');
+
+  calculateForm: FormGroup = new FormGroup({
+    netValue: this.netValue,
+    grossValue: this.grossValue,
+    vatValue: this.vatValue,
+    vatRate: this.vatRate,
+    radioInputData: this.radioInputData
   });
 
+
   constructor(private calculatorService: CalculatorService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  
+  }
 
   calculate(): void {
     this.errorMessage = '';
     this.calculatorService.calculatePurchaseData(this.purchaseDataDto).subscribe(
       (purchaseDataDto) => { this.purchaseDataDto = purchaseDataDto },
-      (error) => { this.errorMessage = error.error}
+      (error) => { this.errorMessage = error.error }
     )
+  }
+
+  disable(): void {
+   
+
   }
 
 }
